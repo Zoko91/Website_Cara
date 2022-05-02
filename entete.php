ed159176922fccdb090ed670652eabb661fab246
zoko91 <josephbeasse@gmail.com>
2022-05-02 20:19:37 +0200

    Third commit : Front index

--- entete.php
+++ entete.php
@@ -7,39 +7,46 @@ session_start();?>
     <meta charset="UTF-8">
     <title>Projet Cara/Visages</title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="images/logo.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/styles.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 
-<body>
-<header id="heads" class="py-4">
+<body >
+<header id="heads" class="pb-4">
     <nav id="navb" class="navbar navbar-expand-md fixed-top navbar-dark bg-dark" style="background-color: rgb(38,41,45);">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <img src="images/logo.png" class="ml-5 mr-2" alt="petit logo d'en haut" style="width: 5%; height: 5%;">
+        <a class="navbar-brand" href="#">
 
-            <div class="collapse navbar-collapse" id="navbarColor01">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Test1 <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Test2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Test3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Test4</a>
-                    </li>
-                </ul>
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
+            <span class="bg-gradient bg-light p-1 rounded text-dark" id="spanb">Projet</span> Transdisciplinaire</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Présentation<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Expérimentations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Analyses des tests</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Résultats</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Conclusion</a>
+                </li>
+            </ul>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
     </nav>
 </header>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php include("entete.php");
 ?>
-<img src="images/Cara_logo.png" alt="Logo Cara" class="mx-auto d-block">
+<br>
+<br>
+<br>
+<div class="text-center" style="background-color: rgb(38,41,45)"><h1>Bienvenue dans le site du Projet <img src="images/C.png" alt="C" style="width: 3%; height: 3%;"><span id="a1">a</span><span id="r">r</span><span id="a2">a</span></h1></div>
+<br>
+<br>
+<img src="images/Cara_logo.png" alt="Logo Cara" class="mx-auto d-block w-50 h-50">
+
 <?php include("footer.php") ?>
