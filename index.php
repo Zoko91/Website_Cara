f25ddc61e5d6f64ecebe7cf0c292c43e9097c45b
Joseph Beasse <94929813+Zoko91@users.noreply.github.com>
2022-05-17 16:57:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php include("entete.php");
 ?>
-<br>
-<br>
-<br>
+
 <div class="text-center" style="background-color: rgb(38,41,45)"><h1>Bienvenue dans le site du Projet <img src="images/C.png" alt="C" style="width: 3%; height: 3%;"><span id="a1">a</span><span id="r">r</span><span id="a2">a</span></h1></div>
 <br>
 <br>
