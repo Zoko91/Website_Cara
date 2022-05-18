96c1a8845c1ebf3cc392e0632c619149f7498c2e
zoko91 <josephbeasse@gmail.com>
2022-05-18 18:21:29 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <br>
 <br>
 <footer>
-    <div class="container-fluid bg-dark p-2">
+    <div id="footer" class="container-fluid bg-dark p-2">
         <div class="row">
             <div class="col text-center"> Fait avec <i class="fa-solid fa-hand-holding-heart"></i> par L'équipe du Projet Cara.</div>
         </div>
