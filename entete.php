2fdf2129fe63e9d1e66378dee54764b64fe78e76
juliettemnr <meunierjuliette62@gmail.com>
2022-05-18 18:38:45 +0200

    cc

--- entete.php
+++ entete.php
@@ -57,7 +57,7 @@ session_start();?>
                             <a class="nav-link  dropdown-item  text-white bg-dark" href="gestion.php">Gestion de Projet</a>
                         </li>
                         <li class="nav-item bg-dark">
-                            <a class="nav-link  dropdown-item  text-white bg-dark" href="#">Conclusion</a>
+                            <a class="nav-link  dropdown-item  text-white bg-dark" href="conclusion.php">Conclusion</a>
                         </li>
                         <div class="dropdown-divider bg-light "></div>
                         <li class="nav-item bg-dark">
