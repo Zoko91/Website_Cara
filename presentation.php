ed4c9bedda127e1ae19234b6d14a5815b76740c9
juliettemnr <meunierjuliette62@gmail.com>
2022-05-18 17:59:18 +0200

    fin

--- presentation.php
+++ presentation.php
@@ -7,13 +7,13 @@
 <div class=container>
 <h3 class="text-center">Notre projet </h3>
 <p>
-Commanditée par M. Ricardo Ron Angevin, chercheur travaillant à Malaga en Espagne et ayant pour domaine de prédilection les interfaces cerveau-machine (BCI), cette étude repose sur l’utilisation du BCI P300-Speller, dont nous disposons d’un exemplaire dans le bâtiment technique de l’école (communément appelé la “forge”).
+Commanditée par M. Ricardo Ron Angevin, chercheur travaillant à Malaga en Espagne et ayant pour domaine de prédilection les interfaces cerveau-machine (BCI : Brain Computer Interface), cette étude repose sur l’utilisation du BCI P300-Speller, dont nous disposons d’un exemplaire dans le bâtiment technique de l’école (communément appelé la “forge”).
 </p>
 <p>
 Les BCI permettent via un casque EEG (électroencéphalogramme) d'afficher des lettres à l'aide d'un clavier virtuel.
 </p>
 <p>
-Cependant, l'utilisation d'un casque BCI n’est pas infaillible, et est donc toujours en cours de développement. Ainsi, en 2011, des recherches menées par Kaufman et al ont montré que l’utilisation d’images de célébrités remplaçant les flashs lumineux avait un impact positif sur les performances du dispositif, et la même conclusion a été observée par Álvaro Fernández-Rodríguez et al en 2019 avec l’utilisation d’images neutres.
+Cependant, l'utilisation d'un casque BCI n’est pas infaillible, et est donc toujours en cours de développement. Ainsi, en 2011, des recherches menées par Kaufman et al ont montré que l’utilisation d’images de célébrités remplaçant les flashs lumineux avait un impact positif sur les performances du dispositif, et la même conclusion a été observée par Álvaro Fernández-Rodríguez et al en 2019 avec l’utilisation d’images neutres (<a style="color:white" href='etatart.php?page=type'>pour en savoir plus...</a>).
 C’est ce désir de comparer les deux méthodes qui a donné naissance au projet Cara.
 </p>
 <h3 class="text-center"> Qu'est-ce qu'une interface cerveau-machine (BCI)? </h3>
