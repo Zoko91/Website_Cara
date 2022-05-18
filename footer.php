8596d07dce9122bce965e6a112cc6345470bf762
Joseph Beasse <94929813+Zoko91@users.noreply.github.com>
2022-05-18 18:16:49 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <br>
 <br>
 <footer>
-    <div class="container-fluid bg-dark">
+    <div class="container-fluid bg-dark p-2">
         <div class="row">
-            <div class="col text-center">Made with ❤ by the CaraTeam</div>
+            <div class="col text-center"> Fait avec <i class="fa-solid fa-hand-holding-heart"></i> par L'équipe du Projet Cara.</div>
         </div>
     </div>
 </footer>
