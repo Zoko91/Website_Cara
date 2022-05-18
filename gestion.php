e6618a3270276d7eef5f7466d0dfaaf2f7837f7a
zoko91 <josephbeasse@gmail.com>
2022-05-18 18:49:45 +0200

    gestion footer et css

--- gestion.php
+++ gestion.php
@@ -6,30 +6,62 @@
     <h1>
         Gestion de Projet
     </h1>
-    <h3>
-        Les outils utilisés
-    </h3>
     <br>
+    <h3>
+        Notre stratégie
+    </h3>
     <div class="container">
-        <p>La stratégie de travail optée par le groupe se rapproche plus de ‘’rushs’’ d’une semaine où une activité précise devait être réalisée avant la mise en commun la semaine d’après.
-            Nous avons également discuté via Zoom avec notre client, à une fréquence de réunion d’environ une semaine sur 2. Cela nous a permis de garder les idées claires sur les objectifs à atteindre et le bon déroulement du projet au cours de ce semestre.</p>
+        <div class="row">
+            <div class="col-9 pl-0">
+                <br><br>
+                <p>La stratégie de travail optée par le groupe se rapproche plus de ‘’rushs’’ d’une semaine où une activité précise devait être réalisée avant la mise en commun la semaine d’après.
+                    Nous avons également discuté via Zoom avec notre client, à une fréquence de réunion d’environ une semaine sur 2. Cela nous a permis de garder les idées claires sur les objectifs à atteindre et le bon déroulement du projet au cours de ce semestre.</p>
+            </div>
+            <div class="col-3">
+                <img src="images/zoom.png" alt="Logo ZOOM" style="width: 100%; height: 100%">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <p> Lors du premier semestre l’équipe a pu apprendre à travailler en autonomie lorsque des tâches étaient confiées entre chaque réunion. Nous avons également réalisé des activités de groupe, comme l’élaboration des claviers, ou des brainstormings mais à l’approche du second semestre, nous délaissions petit à petit cette stratégie de management horizontal pour gagner en efficacité.</p>
+            <br>
+            <p>
+                Nous allons désormais former des groupes de 2 ou 3 élèves qui seront chargés d’une activité précise chaque semaine: réalisation de x tests le mardi, tandis que l’autre groupe fera la rédaction de l’article par exemple.
+                Une répartition bien équilibrée et qui change souvent nous permettra de gagner en efficacité sur ce deuxième semestre.
+            </p>
 
-       <p> Lors de ce premier semestre l’équipe a pu apprendre à travailler en autonomie lorsque des tâches étaient confiées entre chaque réunion. Nous avons également réalisé des activités de groupe, comme l’élaboration des claviers, ou des brainstormings mais à l’approche du second semestre, nous délaissions petit à petit cette stratégie de management horizontal pour gagner en efficacité.</p>
-        <p>
-        Nous allons désormais former des groupes de 2 ou 3 élèves qui seront chargés d’une activité précise chaque semaine: réalisation de x tests le mardi, tandis que l’autre groupe fera la rédaction de l’article par exemple.
-        Une répartition bien équilibrée et qui change souvent nous permettra de gagner en efficacité sur ce deuxième semestre.
-        </p>
-        <p>
-        Nous utilisons l’application Messenger comme un outil de communication instantanée, ce qui nous permet d’avoir un suivi constant des activités de chacun.
-        </p>
-        <p>Google Drive est notre outil de stockage collaboratif qui nous permet de partager et d’accéder à différents travaux ou ressources.</p>
-
-        <p>L’application Discord nous permet de partager des fichiers, des comptes rendus de réunions sur différents canaux et de stocker ces messages. Nous l’utilisons également pour effectuer des réunions à distance.</p>
+        </div>
+        <br>
+        <hr>
+        <br>
+        <div class="row">
+            <div class="col-2">
+                <img src="images/messenger.png" alt="Logo ZOOM" style="width: 70%; height: 70%">
+            </div>
+            <div class="col-10">
+                <p>
+                    Nous utilisons l’application <strong>Messenger</strong> comme un outil de communication instantanée, ce qui nous permet d’avoir un suivi constant des activités de chacun.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <img src="images/drive.png" alt="Logo ZOOM" style="width: 70%; height: 70%">
+            </div>
+            <div class="col-10">
+                <p><strong>Google</strong> Drive est notre outil de stockage collaboratif qui nous permet de partager et d’accéder à différents travaux ou ressources.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <img src="images/discord.png" alt="Logo ZOOM" style="width: 70%; height: 70%">
+            </div>
+            <div class="col-10">
+                <p>L’application <strong>Discord</strong> nous permet de partager des fichiers, des comptes rendus de réunions sur différents canaux et de stocker ces messages. Nous l’utilisons également pour effectuer des réunions à distance.</p>
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="container">
-<h2>TEST TEST</h2>
-</div>
 <?php include("footer.php") ?>
 
