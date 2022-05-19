2da342f81649a9544aeeda78492dde6a8ca339a6
maiwennlegall <101551108+maiwennlegall@users.noreply.github.com>
2022-05-19 13:48:37 +0200

    resultats tableau

---
+++ resultats.php
@@ -0,0 +1,190 @@
+<?php include("entete.php"); ?>
+    <br>
+    <br>
+    <br>
+    <div class=container>
+        <h1 class="text-center">Résultats de l'expérience finale</h1><br/>
+        
+        <section>
+            <h2>Résultats</h2>
+            <table>
+                <tr style="background-color: rgb(0,70,139)">
+                    <td>Sujet</td>
+                    <td>Clavier</td>
+                    <td>Résultat calibration</td>
+                    <td>Nombre de flashs phase on-line</td>
+                    <td>Nombre d'erreurs phase on-line</td>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°2</td>
+                    <td>Célébrité (2)</td>
+                    <td>100% dès le 2nd flash</td>
+                    <td>3</td>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <td>Neutre (1)</td>
+                    <td>100% dès le 2nd flash</td>
+                    <td>3</td>
+                    <td>1</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°3</td>
+                    <td>Célébrité (1)</td>
+                    <td>100% dès le 2nd flash</td>
+                    <td>3</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Neutre (2)</td>
+                    <td>100% dès le 6ème flash</td>
+                    <td>7</td>
+                    <td>0</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color :rgb(139,0,0)">Sujet n°4</td>
+                    <td>Célébrité (2)</td>
+                    <td>100% dès le 5ème flash</td>
+                    <td>6</td>
+                    <td>2</td>
+                </tr>
+                <tr>
+                    <td>Neutre (1)</td>
+                    <td>100% dès le 1er flash</td>
+                    <td>2</td>
+                    <td>0</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°5</td>
+                    <td>Célébrité (1)</td>
+                    <td>100% dès le 1er flash</td>
+                    <td>2</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Neutre (2)</td>
+                    <td>100% dès le 2nd flash</td>
+                    <td>3</td>
+                    <td>0</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°6</td>
+                    <td>Célébrité (2)</td>
+                    <td>100% dès le 4ème flash</td>
+                    <td>5</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Neutre (1)</td>
+                    <td>100% dès le 4ème flash</td>
+                    <td>5</td>
+                    <td>4</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°7</td>
+                    <td>Célébrité (1)</td>
+                    <td>100% dès le 9ème flash</td>
+                    <td>10</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Neutre (2)</td>
+                    <td>100% dès le 3ème flash</td>
+                    <td>4</td>
+                    <td>1</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°8</td>
+                    <td>Célébrité (2)</td>
+                    <td>100% dès le 3ème flash</td>
+                    <td>4</td>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <td>Neutre (1)</td>
+                    <td>100% dès le 2ème flash</td>
+                    <td>3</td>
+                    <td>1</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°9</td>
+                    <td>Célébrité (1)</td>
+                    <td>100% dès le 4ème flash</td>
+                    <td>5</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Neutre (2)</td>
+                    <td>100% dès le 3ème flash</td>
+                    <td>4</td>
+                    <td>0</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°10</td>
+                    <td>Célébrité (2)</td>
+                    <td>100% dès le 4ème flash</td>
+                    <td>5</td>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <td>Neutre (1)</td>
+                    <td>100% dès le 2ème flash</td>
+                    <td>3</td>
+                    <td>1</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°11</td>
+                    <td>Célébrité (1)</td>
+                    <td>100% dès le 4ème flash</td>
+                    <td>5</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Neutre (2)</td>
+                    <td>100% dès le 2ème flash</td>
+                    <td>3</td>
+                    <td>1</td>
+                </tr>
+                </tr>
+                <tr>
+                <tr>
+                    <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°12</td>
+                    <td>Célébrité (2)</td>
+                    <td>100% dès le 2ème flash</td>
+                    <td>3</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Neutre (1)</td>
+                    <td>100% dès le 3ème flash</td>
+                    <td>4</td>
+                    <td>0</td>
+                </tr>
+                </tr>
+            </table>
+            <br>
+            <p><strong>Remarque :</strong> Pour le sujet 1, les signaux n'étants pas satisfaisants, nous n'avons pas pu réaliser l'expérience. </p>
+            <br/>
+            
+        </section>
+    </div>
+<?php include("footer.php") ?>
