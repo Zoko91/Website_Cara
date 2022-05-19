8513b39a1c16380f61144d3f987f523931dbff45
maiwennlegall <101551108+maiwennlegall@users.noreply.github.com>
2022-05-19 23:03:32 +0200

    page résultats

--- resultats.php
+++ resultats.php
@@ -14,35 +14,40 @@
                     <td>Résultat calibration</td>
                     <td>Nombre de flashs phase on-line</td>
                     <td>Nombre d'erreurs phase on-line</td>
+                    <td>Mots sur lesquels les erreurs ont été faites</td>
                 </tr>
                 <tr>
                 <tr>
                     <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°2</td>
                     <td>Célébrité (2)</td>
-                    <td>100% dès le 2nd flash</td>
+                    <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>1</td>
+                    <td>CHEK</td>
                 </tr>
                 <tr>
                     <td>Neutre (1)</td>
-                    <td>100% dès le 2nd flash</td>
+                    <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>1</td>
+                    <td>CCEF</td>
                 </tr>
                 </tr>
                 <tr>
                 <tr>
                     <td rowspan="2" style="background-color : rgb(139,0,0)">Sujet n°3</td>
                     <td>Célébrité (1)</td>
-                    <td>100% dès le 2nd flash</td>
+                    <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 <tr>
                     <td>Neutre (2)</td>
                     <td>100% dès le 6ème flash</td>
                     <td>7</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 </tr>
                 <tr>
@@ -52,12 +57,14 @@
                     <td>100% dès le 5ème flash</td>
                     <td>6</td>
                     <td>2</td>
+                    <td>NOGA et WHEF</td>
                 </tr>
                 <tr>
                     <td>Neutre (1)</td>
                     <td>100% dès le 1er flash</td>
                     <td>2</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 </tr>
                 <tr>
@@ -67,12 +74,14 @@
                     <td>100% dès le 1er flash</td>
                     <td>2</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 <tr>
                     <td>Neutre (2)</td>
-                    <td>100% dès le 2nd flash</td>
+                    <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 </tr>
                 <tr>
@@ -82,12 +91,14 @@
                     <td>100% dès le 4ème flash</td>
                     <td>5</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 <tr>
                     <td>Neutre (1)</td>
                     <td>100% dès le 4ème flash</td>
                     <td>5</td>
                     <td>4</td>
+                    <td>NOGA et EHCF</td>
                 </tr>
                 </tr>
                 <tr>
@@ -97,12 +108,14 @@
                     <td>100% dès le 9ème flash</td>
                     <td>10</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 <tr>
                     <td>Neutre (2)</td>
                     <td>100% dès le 3ème flash</td>
                     <td>4</td>
                     <td>1</td>
+                    <td>ABMI</td>
                 </tr>
                 </tr>
                 <tr>
@@ -112,12 +125,14 @@
                     <td>100% dès le 3ème flash</td>
                     <td>4</td>
                     <td>1</td>
+                    <td>YNGA</td>
                 </tr>
                 <tr>
                     <td>Neutre (1)</td>
                     <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>1</td>
+                    <td>LHNE</td>
                 </tr>
                 </tr>
                 <tr>
@@ -127,12 +142,14 @@
                     <td>100% dès le 4ème flash</td>
                     <td>5</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 <tr>
                     <td>Neutre (2)</td>
                     <td>100% dès le 3ème flash</td>
                     <td>4</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 </tr>
                 <tr>
@@ -142,12 +159,14 @@
                     <td>100% dès le 4ème flash</td>
                     <td>5</td>
                     <td>1</td>
+                    <td>FBRI</td>
                 </tr>
                 <tr>
                     <td>Neutre (1)</td>
                     <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>1</td>
+                    <td>YOFA</td>
                 </tr>
                 </tr>
                 <tr>
@@ -157,12 +176,14 @@
                     <td>100% dès le 4ème flash</td>
                     <td>5</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 <tr>
                     <td>Neutre (2)</td>
                     <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>1</td>
+                    <td>LFNE</td>
                 </tr>
                 </tr>
                 <tr>
@@ -172,12 +193,14 @@
                     <td>100% dès le 2ème flash</td>
                     <td>3</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 <tr>
                     <td>Neutre (1)</td>
                     <td>100% dès le 3ème flash</td>
                     <td>4</td>
                     <td>0</td>
+                    <td>/</td>
                 </tr>
                 </tr>
             </table>
@@ -185,6 +208,29 @@
             <p><strong>Remarque :</strong> Pour le sujet 1, les signaux n'étants pas satisfaisants, nous n'avons pas pu réaliser l'expérience. </p>
             <br/>
             
+        </section>
+        <section>
+            <h2>Analyse des résultats</h2>
+            <p>
+                Tout d'abord, étudions la réussite obtenue lors de "l'écriture" des quatre mots imposés en phase de test on-line.
+                En moyenne, le pourcentage de réussite est de 97,5 % avec le clavier célébrité, contre 94,375% avec le clavier neutre. 
+                On ne distingue donc pas à première vue de réelle différence entre les performances réalisées avec les deux claviers. Néanmoins, 
+                une analyse statistique plus poussée pourra être réalisée, afin de vérifier s'il y a ou non une différence significative.
+            </p>
+            <br/>
+            <img class="image" src="images/reussite.png" alt="Graphique illustrant le pourcentage de réussite moyen de chaque clavier dans l'écriture des mots"/>            
+            <p class="text-center"><em>Figure n°1 : Pourcentage de réussite moyen obtenu avec chaque clavier pour "l'écriture" des mots</em></p>
+            <br/>
+            <p>
+                Nous pouvons tout de même étudier ces résultats sous un autre angle. En observant, pour chaque participant si celui-ci a fait plus d'erreurs
+                sur le clavier célébrité, sur le clavier neutre, ou autant sur les deux.<br/>
+                On remarque ainsi que plus de la moitié des sujets ont fait autant d'erreurs sur les deux claviers. Quatre participants en ont fait 
+                plus sur le clavier neutre que sur le calvier célébrité, et un seul en a fait plus sur le clavier célébrité.
+            </p>
+            <br/>
+            <img class="image" src="images/erreurs.png" alt="Graphique illustrant le clavier avec lequel chaque participant a réalisé le plus d'erreurs"/>            
+            <p class="text-center"><em>Figure n°2 : Clavier avec lequel chaque participant a réalisé le plus d'erreurs</em></p>
+            <br/>
         </section>
     </div>
 <?php include("footer.php") ?>
