2dd3e0b58eee6fdcdd6723abbcf37d32030bebe0
zoko91 <josephbeasse@gmail.com>
2022-05-19 10:31:39 +0200

    contact

--- contact.php
+++ contact.php
@@ -1,6 +1,48 @@
 <?php include("entete.php");
 ?>
-<br>
-<br>
+    <br>
+    <br>
+
+    <div class="text-center"><h2>Formulaire de contact :</h2></div>
+
+    <br>
+    <div class="container mx-auto" style="width:30%;">
+        <fieldset class="border border-light border-2 rounded">
+            <form role="form" id="formParag" class="px-4 py-3" method="post">
+                <label for="email">Votre email</label>
+                <input type="email" style="width: 330px" class="border-danger" name="email" id="email" required><br>
+                <br/>
+                <label>Message</label><span>  </span>
+                <textarea name="message" class="border-danger" id="message" style="height:100px; width: 330px;color: black;" required></textarea><br>
+                <br/>
+                <button type="submit" class="btn btn-danger text-white button">Envoyer</button>
+            </form>
+            <?php
+            if (isset($_POST['message'])) {
+                $retour = mail('jbeasse@ensc.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: '.$_POST['email']);
+                if($retour){
+                    $bandeau = '<div class="alert alert-success text-center"><strong style="color:#0f5132">'."Merci ! </strong>Le mail a bien été envoyé.</div><br>";
+                    $retour = false;
+                    echo $bandeau;
+                }
+            }
+            ?>
+        </fieldset>
+    </div>
+    <div class="offset-md-1 col-12 col-md-6">
+
+    </div>
+
+    <div class="col-12 col-md-7 text-dark fw-bold" id="formulaire">
+        <form method="post">
+
+        </form>
+
+    </div>
+
+
+
+
+
 
 <?php include("footer.php") ?>
