3ca01d933942dd5079c2b19f9d0d18f6af728461
Joseph Beasse <94929813+Zoko91@users.noreply.github.com>
2022-05-24 18:59:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <div class="text-center" style="background-color: rgb(38,41,45)"><h1>Bienvenue dans le site du Projet <img src="images/C.png" alt="C" style="width: 3%; height: 3%;"><span id="a1">a</span><span id="r">r</span><span id="a2">a</span></h1>
 </div>
 <br>
-<div class="text-center"><span style="font-family: 'Grape Nuts', cursive;"><h3>Septembre - Mai 2020</h3></span></div>
+<div class="text-center"><span style="font-family: 'Grape Nuts', cursive;"><h3>Septembre - Mai 2022</h3></span></div>
 
 <br>
 <!--<img src="images/Cara_logo.png" alt="Logo Cara" class="mx-auto d-block w-50 h-50">-->
